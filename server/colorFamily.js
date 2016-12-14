dbd9d69c95ccaca3858a4f724c32e53a93fd98e7
Bruce Paul <brucewpaul@gmail.com>
2016-12-13 17:29:10 -0800

    new color schema

--- server/colorFamily.js
+++ server/colorFamily.js
@@ -3,11 +3,13 @@ var mongoose = require('mongoose');
 colorFamilySchema = mongoose.Schema({
   //Hex color codes only!!
   name: { type: String, unique: true },
-  primary: String,
-  secondary1: String,
-  secondary2: String,
-  tertiary1: String,
-  tertiary2: String,
+  colors: {
+    primary: String,
+    secondary1: String,
+    secondary2: String,
+    tertiary1: String,
+    tertiary2: String,
+  },
   //Establish a baseline for copys/popularity
   copyCount: {type: Number, default: 0},
   likes: {type: Number, default: 0},
