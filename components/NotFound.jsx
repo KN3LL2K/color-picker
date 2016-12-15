a438f6cf0d2b5458afa5697fcbc3854c999dfef4
Zack Winchell <zackwinchell@gmail.com>
2016-12-14 19:51:24 -0800

    (refactor) Add react-router-bootstrap
    - Add register & login to TopBar
    - Change 'Create New' button on TopBar to link

---
+++ components/NotFound.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Col } from 'react-bootstrap';
+var NotFound = (props) => (
+  <Col>
+    <h1 id="NotFound">404 - Not Found</h1>
+  </Col>
+);
+
+module.exports = NotFound;
